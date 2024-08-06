834f21e9ef2c0c2ba9d17d3db59cdeadb8029f4d
Adam Due Hansen <adamduehan@gmail.com>
2024-08-06 17:19:04 +0200

    feat(profile-card): adds more styles.

---
+++ index.tsx
@@ -0,0 +1,7 @@
+export default function Home(): React.JSX.Element {
+  return (
+    <ul>
+      <a href="/profile-card">Profile card</a>
+    </ul>
+  );
+}
--- profile-card.tsx
+++ profile-card.tsx
@@ -8,30 +8,42 @@ export default function ProfileCard(): React.JSX.Element {
         <div className="profile-card__img">
           <img src="https://avatar.iran.liara.run/username?username=Johnny+Rogers" />
         </div>
-        <p>Johnny Rogers</p>
-        <a href="#">johnnyrogers</a>
-        <div className="profile-card__icons">
-          <div>
-            <svg width="24" height="24">
-              <use href="/styles/icons.svg#linkedin" />
-            </svg>
-            <svg width="24" height="24">
-              <use href="/styles/icons.svg#facebook" />
-            </svg>
-            <svg width="24" height="24">
-              <use href="/styles/icons.svg#instagram" />
-            </svg>
-            <svg width="24" height="24">
-              <use href="/styles/icons.svg#website" />
-            </svg>
+        <div className="profile-card__content">
+          <p>Johnny Rogers</p>
+          <a href="#">johnnyrogers</a>
+          <div className="profile-card__icons">
+            <div>
+              <a href="#">
+                <svg width="24" height="24">
+                  <use href="/styles/icons.svg#linkedin" />
+                </svg>
+              </a>
+              <a href="#">
+                <svg width="24" height="24">
+                  <use href="/styles/icons.svg#facebook" />
+                </svg>
+              </a>
+              <a href="#">
+                <svg width="24" height="24">
+                  <use href="/styles/icons.svg#instagram" />
+                </svg>
+              </a>
+              <a href="#">
+                <svg width="24" height="24">
+                  <use href="/styles/icons.svg#website" />
+                </svg>
+              </a>
+            </div>
           </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
+            nihil qui provident, a facere non ipsam unde illum odit autem.
+          </p>
         </div>
-        <p className="profile-card__description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
-          nihil qui provident, a facere non ipsam unde illum odit autem.
-        </p>
-        <button>Follow</button>
-        <button>Message</button>
+        <div className="profile-card__buttons">
+          <a href="#">Follow</a>
+          <button>Message</button>
+        </div>
       </article>
     </>
   );
